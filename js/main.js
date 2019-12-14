3c686c80eb6cb48d2b3809949ece3bf1023b5430
h <h@hs-MacBook-Pro.local>
2019-12-14 17:16:15 +1300

    touch up

--- js/main.js
+++ js/main.js
@@ -1,3 +1,13 @@
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+  anchor.addEventListener('click', function (e) {
+      e.preventDefault();
+
+      document.querySelector(this.getAttribute('href')).scrollIntoView({
+          behavior: 'smooth'
+      });
+  });
+});
+
 (function ($) {
 "use strict";
 // TOP Menu Sticky
